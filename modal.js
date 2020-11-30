879683c59d9144122c4f6b4339386ad312ac5ac6
tobmordt <71451532+tobmordt@users.noreply.github.com>
2020-11-30 16:16:47 +0100

    button contact

--- modal.js
+++ modal.js
@@ -13,9 +13,14 @@ var textEmail = document.getElementById("email");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
-    textMessage.value = "";
-    textEmail.value = "";
+    if (textEmail.value==="" || textMessage.value==="") {
+        alert("Please complete the form.");
+    }
+    else {
+        modal.style.display = "block";
+        textMessage.value = "";
+        textEmail.value = "";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -25,7 +30,7 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
